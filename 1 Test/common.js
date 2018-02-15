958eec08aaf306975c49579d2fd12216dd95954a
sashakobzeww <sashakobzeww@gmail.com>
2018-02-15 15:51:28 +0200

    Код умеет рассчитывать определитель матрицы, но он громоздкий

--- 1 Test/common.js
+++ 1 Test/common.js
@@ -12,3 +12,16 @@ for(i = 0; i < 9; i++) {
 	var random = Math.round(Math.random() * 10);
 	matrix2[i] = $("#matrix2").find("td").eq(i).text(random);
 }
+
+var determinant = 
+	($("#matrix1_td1").text() * $("#matrix1_td5").text() * $("#matrix1_td9").text()) + 
+	($("#matrix1_td4").text() * $("#matrix1_td8").text() * $("#matrix1_td3").text()) + 
+	($("#matrix1_td7").text() * $("#matrix1_td2").text() * $("#matrix1_td6").text()) - 
+	($("#matrix1_td7").text() * $("#matrix1_td5").text() * $("#matrix1_td3").text()) - 
+	($("#matrix1_td4").text() * $("#matrix1_td2").text() * $("#matrix1_td9").text()) - 
+	($("#matrix1_td1").text() * $("#matrix1_td8").text() * $("#matrix1_td6").text());
+
+$(".determinantValue").text(determinant);
+
+/*Придумать как уменьшить громоздкость формулы. Наверняка объекты и методы
+смогут помочь*/
