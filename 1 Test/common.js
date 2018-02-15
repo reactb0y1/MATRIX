8cb53c9224f1727123c715d798984192d5c405b5
sashakobzeww <sashakobzeww@gmail.com>
2018-02-15 15:03:46 +0200

    Небольшие правки в предыдущее

--- 1 Test/common.js
+++ 1 Test/common.js
@@ -1,16 +1,14 @@
-/*У меня есть функция, которая может наполнять случайными числами массив.
-У меня есть доступ к каждой из таблицы-матрицы. Как мне напонлнить мои таблицы
-случайными числами?*/
-var matrix1Text = $("#matrix1").find("td").text();
-var matrix2Text = $("#matrix2").find("td").text();
+// Создаём переменные, дающие доступ к к значению каждой ячейки матрицы
+var matrix1 = $("#matrix1").find("td").text();
+var matrix2 = $("#matrix2").find("td").text();
 
+// Наполняем случайными числами ячейки
 for(i = 0; i < 9; i++) {
-	var random2 = Math.round(Math.random() * 10);
-	matrix1Text[i] = $("#matrix1").find("td").eq(i).text(random2);
-	matrix2Text[i] = $("#matrix2").find("td").eq(i).text(random2);
+	var random = Math.round(Math.random() * 10);
+	matrix1[i] = $("#matrix1").find("td").eq(i).text(random);
 }
 
 for(i = 0; i < 9; i++) {
-	var random2 = Math.round(Math.random() * 10);
-	matrix2Text[i] = $("#matrix2").find("td").eq(i).text(random2);
+	var random = Math.round(Math.random() * 10);
+	matrix2[i] = $("#matrix2").find("td").eq(i).text(random);
 }
